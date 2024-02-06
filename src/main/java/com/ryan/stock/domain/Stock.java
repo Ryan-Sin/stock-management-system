01969fb1ea12c1916992edf3b618c99e73a1b362
Ryan-Sin <ryansin937@gmail.com>
2024-02-07 05:36:11 +0900

    feat: 낙관적 동시성제어 적용(OptimisticLock Lock)

--- src/main/java/com/ryan/stock/domain/Stock.java
+++ src/main/java/com/ryan/stock/domain/Stock.java
@@ -1,9 +1,6 @@
 package com.ryan.stock.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
@@ -15,6 +12,8 @@ public class Stock {
     private Long id;
     private Long productId;
     private Long quantity;
+    @Version
+    private Long version;
 
     public Stock() {
     }
--- src/main/java/com/ryan/stock/repository/StockRepository.java
+++ src/main/java/com/ryan/stock/repository/StockRepository.java
@@ -5,10 +5,15 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from Stock s where s.id = :id")
-    Stock findByIdWithPessimisticLock(Long id);
+    Stock findByIdWithPessimisticLock(@Param("id") Long id);
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(@Param("id") Long id);
 }
