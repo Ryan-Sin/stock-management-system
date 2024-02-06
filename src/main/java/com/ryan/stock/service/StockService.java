494afc4b0889ee390a4132946d0b93bd0e93b00c
Ryan-Sin <ryansin937@gmail.com>
2024-02-07 03:32:23 +0900

    feat: synchronized 키워드를 활용한 동시성 이슈 해결

--- src/main/java/com/ryan/stock/service/StockService.java
+++ src/main/java/com/ryan/stock/service/StockService.java
@@ -13,8 +13,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity){
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity){
         /**
          * 재고 조회
          * 재고를 감소시킨 뒤
