3f620b471ae92b3f53527d2471c11ff6ea2daed1
Ryan-Sin <ryansin937@gmail.com>
2024-02-07 03:26:53 +0900

    fix: 멀티 스레드 동시 요청 누락 코드 수정

--- src/test/java/com/ryan/stock/service/StockServiceTest.java
+++ src/test/java/com/ryan/stock/service/StockServiceTest.java
@@ -28,10 +28,10 @@ class StockServiceTest {
         stockRepository.saveAndFlush(new Stock(1L, 100L));
     }
 
-    @AfterEach
-    public void afterEach() {
-        stockRepository.deleteAll();
-    }
+//    @AfterEach
+//    public void afterEach() {
+//        stockRepository.deleteAll();
+//    }
 
     @Test
     public void 재고감소() {
@@ -51,7 +51,7 @@ class StockServiceTest {
      * @description 레이스 컨디션 문제가 발생
      */
     @Test
-    public void 동시_100개의_요청() {
+    public void 동시_100개의_요청() throws InterruptedException {
         // given(준비): 어떠한 데이터가 준비되었을 때
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(32);
@@ -68,7 +68,8 @@ class StockServiceTest {
             });
         }
 
-        latch.countDown();
+        latch.await();
+
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         // then(검증): 어떠한 결과가 나와야 한다.
