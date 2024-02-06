bfe64ff9b2b014e6b49c6bebaffd136af84c4d7d
Ryan-Sin <ryansin937@gmail.com>
2024-02-07 03:48:34 +0900

    feat: 비관적 동시성제어 적용(Pessimistic Lock)

---
+++ src/main/java/com/ryan/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,24 @@
+package com.ryan.stock.service;
+
+import com.ryan.stock.domain.Stock;
+import com.ryan.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+
+        stockRepository.save(stock);
+    }
+}
