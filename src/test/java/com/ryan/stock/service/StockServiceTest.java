c69af73a6a42f7fbedda57ab3aa4460a381530c8
Ryan-Sin <ryansin937@gmail.com>
2024-02-07 03:16:04 +0900

    feat: 멀티 스레드 테스트 코드 작성(race condition 발생)

--- src/test/java/com/ryan/stock/service/StockServiceTest.java
+++ src/test/java/com/ryan/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -39,5 +43,36 @@ class StockServiceTest {
 
         // then(검증): 어떠한 결과가 나와야 한다.
         assertEquals(99, stock.getQuantity());
+    }
+
+
+    /**
+     * @author Ryan
+     * @description 레이스 컨디션 문제가 발생
+     */
+    @Test
+    public void 동시_100개의_요청() {
+        // given(준비): 어떠한 데이터가 준비되었을 때
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when(실행): 어떠한 함수를 실행하면
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+               try{
+                   stockService.decrease(1L, 1L);
+               }finally {
+                   latch.countDown();
+               }
+            });
+        }
+
+        latch.countDown();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // then(검증): 어떠한 결과가 나와야 한다.
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
     }
 }
